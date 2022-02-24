c96e6b18c1d62eac75ddbf71511b41fdb7e5cc75
Easton Bolinger <bolinger@ksu.edu>
2022-02-24 13:13:36 -0600

    All screens added from Game Architecture tutorial

---
+++ Screens/PlayerEventIndexEventArgs.cs
@@ -0,0 +1,20 @@
+﻿/*
+    This screen is an adapted version of the one provided by Nathan Bean in the Game Architecture Tutorial to suit this game
+*/
+using System;
+using Microsoft.Xna.Framework;
+
+namespace ChickenFarm.Screens
+{
+    // Custom event argument which includes the index of the player who
+    // triggered the event. This is used by the MenuEntry.Selected event.
+    public class PlayerIndexEventArgs : EventArgs
+    {
+        public PlayerIndex PlayerIndex { get; }
+
+        public PlayerIndexEventArgs(PlayerIndex playerIndex)
+        {
+            PlayerIndex = playerIndex;
+        }
+    }
+}
